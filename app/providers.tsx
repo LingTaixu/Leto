a440fa54cffe816618f17596ac52a9994214dfab
CHENSS\chenss <taixuling@gmail.com>
2026-09-24 02:21:05 +0800

    feat: 加入vercel埋点

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { config } from "@/lib/rainbowkit";
 import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/next";
 import type { ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
-import { config } from "@/lib/rainbowkit";
-
 const queryClient = new QueryClient();
 
 /**
@@ -19,6 +19,7 @@ export function Providers({ children }: { children: ReactNode }) {
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider theme={lightTheme({ accentColor: "#FFD23F" })}>
           {children}
+          <Analytics />
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
